70f03ea89469741d167c899c035613aaa3f85ecb
Michail83 <zhadovichmichail@gmail.com>
2021-11-16 16:30:38 +0300

    Add project files.

---
+++ BookingSystem.WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BookingSystem.WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
