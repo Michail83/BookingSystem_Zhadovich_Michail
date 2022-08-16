31e08726238de91599f405a57fbc070876940501
Mihail Zhadovich <zhadovichmichail@gmail.com>
2022-08-16 19:13:45 +0300

    change callback path to custom

--- BookingSystem.WEB/Startup.cs
+++ BookingSystem.WEB/Startup.cs
@@ -57,7 +57,7 @@ namespace BookingSystem.WEB
                      options.ClientId = authData["ClientId"];
                      options.ClientSecret = authData["ClientSecret"];
                      options.SignInScheme = IdentityConstants.ExternalScheme;
-                     //options.CallbackPath = "/signin-google";
+                     options.CallbackPath = "/signin-rnuto45";
                      options.CorrelationCookie.SameSite = SameSiteMode.None;
 
                      options.Events.OnRedirectToAuthorizationEndpoint = MakeHttps;
